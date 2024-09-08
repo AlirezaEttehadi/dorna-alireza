d71ac93bfcb7b1a7e42c7884cc02d0c0a4f479d4
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-09-09 01:54:10 +0330

    fix: again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     const websiteMusic = new Audio("/sounds/music.mp3");
     const handleInteraction = () => {
       if (websiteMusic) {
+        websiteMusic.loop = true;
         websiteMusic.play();
       }
     };
